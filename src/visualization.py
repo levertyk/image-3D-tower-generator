23ae74e083963a870c8eb648d193ae48f25ae999
Keenan Leverty <91560030+levertyk@users.noreply.github.com>
2024-10-01 11:05:20 -0500

    Refactor visualization code for tower rendering

--- src/visualization.py
+++ src/visualization.py
@@ -6,3 +6,16 @@ def init_window(width, height):
     pygame.init()
     pygame.display.set_mode((width, height), pygame.OPENGL | pygame.DOUBLEBUF)
     glEnable(GL_DEPTH_TEST)
+    
+def render_towers(towers):
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    for tower in towers:
+        glBegin(GL_QUADS)
+        #TODO draw towers
+        glEnd()
+        
+def main_loop(towers):
+    init_window(800, 600)
+    while True:
+        render_towers(towers)
+        pygame.time.wait(10) #max frame rate is 10 per second for rendering
