a7e35ef97e5d3d567cb91b6eb3e2a7b5f0b0d3ca
Keenan Leverty <91560030+levertyk@users.noreply.github.com>
2024-10-01 13:59:12 -0500

    Refactor main.py to load an image and generate towers for visualization

--- src/main.py
+++ src/main.py
@@ -1,4 +1,7 @@
-from gui import run_gui # type: ignore
+from gui import run_gui
+from tower_generator import generate_towers
+from image_processing import load_image
 
 if __name__ == "__main__":
-    run_gui()  # Start the application
+    image = load_image("data\\frog.jpg", (100, 100))  # Load the image
+    run_gui(generate_towers(image, 0, 0, 10))  # Start the application
