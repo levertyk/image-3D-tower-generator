750aaf32181fbe464b28cf7d0418b7be0893081c
Keenan Leverty <91560030+levertyk@users.noreply.github.com>
2024-10-01 10:44:27 -0500

    Color functions

--- image_processing.py
+++ image_processing.py
@@ -5,3 +5,10 @@ def load_image(filepath, new_size):
     img = Image.open(filepath)
     img = img.resize(new_size)
     return np.array(img)
+
+def sample_color(image_data, x, y):
+    return image_data[y, x]
+
+def average_color(image_data, x, y, size):
+    region = image_data[y:y+size, x:x+size]
+    return np.mean(region, axis=(0, 1))
